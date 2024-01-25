2943bad1941c46923ae3151c3ee60bae05144baa
Rushabh Potarwar <I24.Rushabh.P@FinIQ-SP.com>
2024-01-25 11:06:26 +0530

    first-commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularCalculator';
+
+  toshow = ''
+  currvalue=''
+  flagb=false;
+  ptrflag=false;
+
+  writetoinput(value: string){
+    
+    if(this.currvalue===''&& value==='.'){
+      alert("Invalid Input");
+      return;
+    }
+
+    if((this.currvalue[this.currvalue.length-1]==='+' || this.currvalue[this.currvalue.length-1]==='-' || this.currvalue[this.currvalue.length-1]==='*' || this.currvalue[this.currvalue.length-1]==='/') && (value === '+'  || value==='-' || value === '*' ||  value === '/')){
+      alert("Invalid Input");
+    
+    }else{
+
+      this.currvalue = this.currvalue + value
+    }
+
+    if((this.toshow ==='' && this.currvalue[this.currvalue.length-1]==='/')|| (this.toshow ==='' && this.currvalue[this.currvalue.length-1]==='*')){
+      alert("Invalid Input");
+      this.flagb=true;
+    }
+    else{
+
+      this.toshow = this.currvalue
+    }
+
+    if(this.flagb===true){
+      this.toshow='';
+      this.currvalue='';
+      this.flagb=false;
+    }
+  }
+
+  equals(){
+    if(this.toshow[this.toshow.length-1]=='/' || this.toshow[this.toshow.length-1]=='+' || this.toshow[this.toshow.length-1]=='*' || this.toshow[this.toshow.length-1]=='-'){
+      alert("Invalid Input");
+    }
+    this.toshow = eval(this.currvalue)
+    this.currvalue = this.toshow
+  }
+
+  clear(){
+    this.currvalue=''
+    this.toshow= ''
+  }
+
+  back(){
+    this.currvalue = this.currvalue.slice(0,-1)
+    if(this.currvalue==''){this.currvalue=''}
+    this.toshow =this.currvalue
+  }
+
+  calcvalue(solve: any){
+    this.toshow = eval(solve) 
+  }
+
+
+}
